0b7de274acaf574a3598fbca323cca27403926bc
vidhushi222 <78249793+vidhushi222@users.noreply.github.com>
2021-12-04 21:26:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function myroom(){
+    window.location="myroom.html"
+}
+
+function kitchen(){
+    window.location="kitchen.html"
+}
+
+function masterbedroom(){
+    window.location="masterroom.html"
+}
+
+function livingroom(){
+    window.location="livingroom.html"
+}
+
+function balcony(){
+    window.location="balcony.html"
+}
+
+function back(){
+    window.location="index.html"
+}
